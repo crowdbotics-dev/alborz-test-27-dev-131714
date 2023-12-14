57cd4772cbee340ebecaffea49c216e2d20b5a12
Crowdbotics <engineering@crowdbotics.com>
2023-12-14 18:22:33 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "alborz-test-27-dev-131714",
+  url: "https://alborz-test-27-dev-131714.botics.co",
+  api: "https://alborz-test-27-dev-131714.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
